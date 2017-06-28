3f0a8a5f32c754e1d600338756e7cfc9cbe3bf13
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2017-06-28 17:09:32 +0900

    Add matchByReq

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ const test = require('tape');
 const {
   getPathParameter,
   matchByPath,
+  matchByReq,
 } = require('..');
 
 test('test getPathParameter', t => {
@@ -75,5 +76,34 @@ test('test matchByPath with callback', t => {
   matchByPath(resources, '/xxx', (err, result) => {
     t.error(err);
     t.equal(result, null);
+  });
+});
+
+test('test matchByReq', t => {
+  t.plan(6);
+
+  const resources = [
+    {
+      id: "1",
+      path: "/",
+      resourceMethods: {
+        GET: {
+          methodIntegration: {
+          },
+        },
+      },
+    },
+  ];
+
+  matchByReq(resources, { path: '/xxx', method: 'GET' }, (err, result) => {
+    t.error(err);
+    t.equal(result, null);
+  });
+
+  matchByReq(resources, { path: '/', method: 'GET' }, (err, result) => {
+    t.error(err);
+    t.ok(result);
+    t.ok(result.resourceMethod);
+    t.ok(result.resourceMethod.methodIntegration);
   });
 });
